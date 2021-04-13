0023c415fdf7b93c77acd7032a23075a11ceb370
255934 <34775146+255934@users.noreply.github.com>
2021-04-13 22:23:18 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,19 +1,11 @@
-//HEADER FILES
-#include<stdio.h>//Use for standard I/O Operation
 
-#include<conio.h>//Use for delay(),getch(),gotoxy(),etc.
-#include<ctype.h>//se for toupper(), tolower(),etc
-#include<string.h>//Use for strcmp(),strcpy(),strlen(),etc
-#include<stdlib.h>
-#include "covid_management.h"
 
 
 
 
 //HEADER FILES
 #include<stdio.h>//Use for standard I/O Operation
-#include<windows.h>
-#include<conio.h>//Use for delay(),getch(),gotoxy(),etc.
+
 #include<ctype.h>//se for toupper(), tolower(),etc
 #include<string.h>//Use for strcmp(),strcpy(),strlen(),etc
 #include<stdlib.h>
